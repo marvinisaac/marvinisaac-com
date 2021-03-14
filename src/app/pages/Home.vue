4ba89ce08db2631a6c9561b735e2fc6f4737171f
Marvin Isaac <marvin@marvinisaac.com>
2021-03-14 10:10:55 +0800

    :lipstick: Create v1 of layout

--- src/app/pages/Home.vue
+++ src/app/pages/Home.vue
@@ -1,8 +1,42 @@
 <template>
     <div class="container
         flex items-center max-w-screen-sm mx-auto my-0 p-4 h-screen">
+
         <div class="card-container
-            bg-white mx-auto my-0 rounded shadow-lg w-full">
+            bg-white mx-auto my-0 relative rounded-md shadow-2xl w-full">
+
+            <div class="card
+                absolute flex flex-auto h-full items-end left-0 top-0 w-full">
+
+                <div class="text-container
+                    pb-4 pl-4
+                    md:pb-6 md:pl-6">
+
+                    <h1 class="leading-8 text-xl">
+                        <a href="https://linkedin.com/in/marvinisaac">
+                            <span class="text-sm">✉️ </span>
+                            Marvin Isaac
+                        </a>
+                    </h1>
+
+                    <p class="leading-8 text-sm">
+                        <span class="mr-4">
+                            <a href="https://github.com/marvinisaac">⌨️ Developer</a>
+                        </span>
+                    </p>
+
+                    <p class="leading-8 text-sm">
+                        <span>
+                            <a href="#">✏️ Writer</a>
+                        </span>
+                    </p>
+                </div>
+
+                <div class="image-container
+                    flex-grow self-center">
+                    <img src="https://hhl.sfo2.cdn.digitaloceanspaces.com/marvinisaac/brain-hemispheres.svg">
+                </div>
+            </div>
         </div>
     </div>
 </template>
