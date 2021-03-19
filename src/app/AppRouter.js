2166d153311d2bbee3f5767d47edd9fccbb7faf1
Marvin Isaac <marvin@marvinisaac.com>
2021-03-19 10:09:51 +0800

    :sparkles: Create 404 page

--- src/app/AppRouter.js
+++ src/app/AppRouter.js
@@ -1,13 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/Home.vue'
+import NotFound from './pages/NotFound.vue'
 
 const routes = [
     {
         path: '/',
         component: Home
     }, {
+        path: '/not-found',
+        component: NotFound
+    }, {
         path: '/:pathMatch(.*)*',
-        redirect: '/'
+        redirect: '/not-found'
     }
 ]
 
---
+++ src/app/pages/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="align-center flex flex-col h-screen justify-center text-center w-full">
+        <h1 class="leading-loose text-white text-2xl uppercase
+            md:text-5xl">
+            Page not found
+        </h1>
+        <div class="bg-white mx-auto my-4 rounded px-4
+            md:w-1/6">
+            <p class="leading-8 text-sm">
+                <a href="/">
+                    Go back to home page
+                </a>
+            </p>
+        </div>
+    </div>
+</template>
