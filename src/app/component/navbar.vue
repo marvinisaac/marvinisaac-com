bc67d483181655161e6b99311e04c8f303bb9d60
Marvin Isaac <marvin@marvinisaac.com>
2021-03-30 09:00:57 +0800

    :construction: Add navbar to timeline

---
+++ src/app/component/navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bg-white border-b border-gray-300 h-16 fixed left-0 top-0 w-full z-50">
+        <div class="flex h-full justify-between max-w-screen-sm mx-auto">
+            <div class="h-full p-2">
+                <router-link class="h-full"
+                    to="/">
+                    <img class="h-full"
+                        src="https://one.sgp1.cdn.digitaloceanspaces.com/marvinisaac/m-small.jpg">
+                </router-link>
+            </div>
+
+            <div class="flex h-full p-6">
+                <a class="h-full ml-4"
+                    href="https://github.com/marvinisaac">
+                    <img class="h-full"
+                        src="https://one.sgp1.cdn.digitaloceanspaces.com/marvinisaac/github.svg">
+                </a>
+                <a class="h-full ml-4"
+                    href="https://linkedin.com/in/marvinisaac">
+                    <img class="h-full"
+                        src="https://one.sgp1.cdn.digitaloceanspaces.com/marvinisaac/linkedin.svg">
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
--- src/app/pages/Timeline/Timeline.vue
+++ src/app/pages/Timeline/Timeline.vue
@@ -1,45 +1,37 @@
 <template>
-    <div class="max-w-screen-sm mx-auto p-4">
-        <div class="flex justify-end mb-2">
-            <my-button v-if="hasTag"
-                class="italic font-bold"
-                text="Remove Filter"
-                url="/timeline">
-            </my-button>
-            <my-button class="italic ml-2"
-                text="Back to Home"
-                url="/">
-            </my-button>
-        </div>
-        <img v-if="!posts"
-            class="max-w-screen-sm mx-auto"
-            src="https://one.sgp1.cdn.digitaloceanspaces.com/marvinisaac/loading.svg">
-        <template v-else>
-            <template v-for="post in posts" :key="post.id">
-                <bleep v-if="post.type === 'bleep'"
-                    :id="post.id"
-                    :created="post.date_created"
-                    :body="post.body"
-                    :tag="post.tag">
-                </bleep>
+    <div class="relative">
+        <navbar></navbar>
+        <div class="max-w-screen-sm mx-auto p-4 pt-20">
+            <img v-if="!posts"
+                class="max-w-screen-sm mx-auto"
+                src="https://one.sgp1.cdn.digitaloceanspaces.com/marvinisaac/loading.svg">
+            <template v-else>
+                <template v-for="post in posts" :key="post.id">
+                    <bleep v-if="post.type === 'bleep'"
+                        :id="post.id"
+                        :created="post.date_created"
+                        :body="post.body"
+                        :tag="post.tag">
+                    </bleep>
 
-                <blog v-if="post.type === 'blog'"
-                    :id="post.id"
-                    :created="post.date_created"
-                    :title="post.title"
-                    :body="post.body"
-                    :tag="post.tag">
-                </blog>
+                    <blog v-if="post.type === 'blog'"
+                        :id="post.id"
+                        :created="post.date_created"
+                        :title="post.title"
+                        :body="post.body"
+                        :tag="post.tag">
+                    </blog>
 
-                <image-post v-if="post.type === 'image'"
-                    :id="post.id"
-                    :created="post.date_created"
-                    :title="post.title"
-                    :body="post.body"
-                    :tag="post.tag">
-                </image-post>
+                    <image-post v-if="post.type === 'image'"
+                        :id="post.id"
+                        :created="post.date_created"
+                        :title="post.title"
+                        :body="post.body"
+                        :tag="post.tag">
+                    </image-post>
+                </template>
             </template>
-        </template>
+        </div>
     </div>
 </template>
 
@@ -48,15 +40,15 @@ import Bleep from '../Post/Type/Bleep/Bleep.vue'
 import Blog from '../Post/Type/Blog.vue'
 import ImagePost from '../Post/Type/Image/Image.vue'
 import { useRoute } from 'vue-router'
-import myButton from './../../component/button.vue'
 import meta from './../../../app/AppMeta.js'
+import Navbar from './../../component/navbar.vue'
 
 export default {
     components: {
         Bleep,
         Blog,
         ImagePost,
-        myButton
+        Navbar
     },
     data: () => ({
         hasTag: false,
