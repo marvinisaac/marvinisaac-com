7862543cbe23cb18cb38c2aa3dfe05f68b7ee077
Marvin Isaac <marvin@marvinisaac.com>
2021-04-19 05:50:43 +0800

    :bug: Tags in a post should redirect to timeline

--- src/app/pages/Post/Partial/TagList.vue
+++ src/app/pages/Post/Partial/TagList.vue
@@ -2,7 +2,7 @@
     <div class="flex items-center p-4
         md:px-16">
         <span v-for="tagName in tag"
-            @click="tagAdd(tagName)"
+            @click="goToTimeline(tagName)"
             :key="tagName"
             class="cursor-pointer inline-block italic mx-1 text-xs text-gray-500
                 hover:underline">
@@ -17,8 +17,18 @@ export default {
         tag: Array
     },
     methods: {
-        tagAdd(tag) {
+        goToTimeline(tag) {
             this.$store.commit('tagAdd', tag)
+            let tags = this.$store.state.tags
+            let routeOptions = {
+                path: '/timeline'
+            }
+            if (tags.length > 0) {
+                routeOptions.query = {
+                    tag: tags.join(',')
+                }
+            }
+            this.$router.push(routeOptions)
         }
     }
 }
