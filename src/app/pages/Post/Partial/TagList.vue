9e8354c54c120df637db15a3fa9b3496dd188b3c
Marvin Isaac <marvin@marvinisaac.com>
2021-04-21 08:18:54 +0800

    :construction: Tags from a post redirects to timeline

--- src/app/pages/Post/Partial/TagList.vue
+++ src/app/pages/Post/Partial/TagList.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex items-center p-4
         md:px-16">
-        <span v-for="tagName in tag"
-            @click="tagAdd(tagName)"
+        <router-link v-for="tagName in tag"
             :key="tagName"
+            :to="`/timeline?tag=${tagName}`"
             class="cursor-pointer inline-block italic mx-1 text-xs text-gray-500
                 hover:underline">
             #{{ tagName }}
-        </span>
+        </router-link>
     </div>
 </template>
 
@@ -15,11 +15,6 @@
 export default {
     props: {
         tag: Array
-    },
-    methods: {
-        tagAdd(tag) {
-            this.$store.commit('tagAdd', tag)
-        }
     }
 }
 </script>
